0356e5272580a786a8763686ebdd1555e6fff2ea
Hariharan <hariharan17kpm@gmail.com>
2025-02-17 23:43:04 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const placesRoutes = require("./routes/places-routes");
+
+const app = express();
+
+app.use("/api/places", placesRoutes);
+
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+  }
+});
+
+app.listen(5000);
